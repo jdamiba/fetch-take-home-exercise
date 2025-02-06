03787c02fef88f5bf3b43fe9851fdde935280c36
Joseph Damiba <jdamiba@gmail.com>
2025-02-05 23:05:26 -0800

    setup files

---
+++ app.test.js
@@ -0,0 +1,54 @@
+// app.test.js
+const request = require("supertest");
+const app = require("./server");
+const hash = require("object-hash");
+
+const exampleJSON = {
+  retailer: "Target",
+  purchaseDate: "2022-01-01",
+  purchaseTime: "13:01",
+  items: [
+    {
+      shortDescription: "Mountain Dew 12PK",
+      price: "6.49",
+    },
+    {
+      shortDescription: "Emils Cheese Pizza",
+      price: "12.25",
+    },
+    {
+      shortDescription: "Knorr Creamy Chicken",
+      price: "1.26",
+    },
+    {
+      shortDescription: "Doritos Nacho Cheese",
+      price: "3.35",
+    },
+    {
+      shortDescription: "   Klarbrunn 12-PK 12 FL OZ  ",
+      price: "12.00",
+    },
+  ],
+  total: "35.35",
+};
+
+describe("POST /receipts/process", () => {
+  it("should return an ID that is the object hash of the JSON, and the correct points", async () => {
+    const res = await request(app)
+      .post("/receipts/process")
+      .send(exampleJSON)
+      .expect("Content-Type", /html/)
+      .expect(200);
+
+    expect(res.text).toBe(hash(exampleJSON));
+  });
+
+  it("should return the correct points", async () => {
+    const res2 = await request(app)
+      .get(`/receipts/${hash(exampleJSON)}/points`)
+      .expect("Content-Type", /json/)
+      .expect(200);
+
+    expect(res2.text).toBe(`{"points":28}`);
+  });
+});
---
+++ server.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const hash = require("object-hash");
+const app = express();
+const moment = require("moment");
+const { z } = require("zod");
+
+const receiptsMap = {};
+
+const schema = z.object({
+  retailer: z.string(),
+  purchaseDate: z.string(),
+  purchaseTime: z.string(),
+  items: z.array(z.object({ shortDescription: z.string(), price: z.string() })),
+  total: z.string(),
+});
+
+app.use(express.json());
+
+function processReceiptsHandler(req, res) {
+  const result = schema.safeParse(req.body);
+  if (!result.success) {
+    return res.status(400).json({ error: "The receipt is invalid" });
+  }
+  receiptsMap[hash(req.body)] = req.body;
+  res.send(hash(req.body));
+}
+
+function pointsHandler(req, res) {
+  if (req.params.id in receiptsMap) {
+    let points = calculatePoints(receiptsMap[req.params.id]);
+
+    res.json({ points: points });
+  } else {
+    res.send("No receipt found for that ID.");
+  }
+}
+
+function calculatePoints(receipt) {
+  let points = 0;
+
+  let count = receipt["retailer"].match(/[a-zA-Z0-9]/g);
+
+  // One point for every alphanumeric character in the retailer name
+  points += count.length;
+
+  // 50 points if the total is a round dollar amount with no cents.
+  if (receipt["total"] % 1 == 0) {
+    points += 50;
+  }
+
+  // 25 points if the total is a multiple of 0.25.
+  if (receipt["total"] % 0.25 == 0) {
+    points += 25;
+  }
+
+  receipt["items"].forEach((item, count) => {});
+
+  receipt["items"].forEach((item, count) => {
+    // 5 points for every two items on the receipt.
+    if (count % 2 == 1) {
+      points += 5;
+    }
+    const trimmedStr = item["shortDescription"].trim(); // "HelloWorld!"
+
+    //If the trimmed length of the item description is a multiple of 3,
+    // multiply the price by 0.2 and round up to the nearest integer.
+    // The result is the number of points earned.
+    if (trimmedStr.length % 3 == 0) {
+      points += Math.ceil(Number(item["price"]) * 0.2);
+    }
+  });
+
+  if (receipt["purchaseDate"]) {
+    const date = new Date(receipt["purchaseDate"]);
+    const day = date.getUTCDate();
+
+    //6 points if the day in the purchase date is odd.
+    if (day % 2 == 1) {
+      points += 6;
+    }
+
+    const startTime = moment("14:00", "hh:mm");
+    const endTime = moment("16:00", "hh:mm");
+    const purchaseTime = moment(receipt["purchaseTime"], "hh:mm");
+    let inBetween = purchaseTime.isBetween(startTime, endTime);
+
+    // 10 points if the time of purchase is after 2:00pm and before 4:00pm.
+    if (inBetween) {
+      points += 10;
+    }
+  }
+
+  return points;
+}
+
+app.post("/receipts/process", processReceiptsHandler);
+
+app.get("/receipts/:id/points", pointsHandler);
+
+module.exports = app;
